ad1ad1f6599c821660b65c2976fa51e63706edcb
Caio Campos <caiodemoreno@gmail.com>
2026-08-28 01:09:21 -0300

    fix: modal de atualizacao sem listar novidades e melhorias visuais

--- check_types.ps1
+++
@@ -1,5 +0,0 @@
-$result = node node_modules/typescript/bin/tsc --noEmit --pretty false 2>&1
-"---TSC OUTPUT START---" | Out-File -FilePath tsc_result.txt -Encoding utf8
-$result | Out-File -FilePath tsc_result.txt -Append -Encoding utf8
-"---TSC OUTPUT END---" | Out-File -FilePath tsc_result.txt -Append -Encoding utf8
-"EXITCODE=$LASTEXITCODE" | Out-File -FilePath tsc_result.txt -Append -Encoding utf8
